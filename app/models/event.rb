e072dc38d26d6a15a0f97f8dd8d27aaa7144f274
ignaciofigueroa <ijfigueroa@miuandes.cl>
2019-04-03 07:20:23 -0300

    Event date validation ready

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,10 @@
 class Event < ApplicationRecord
   belongs_to :event_venue
   has_many :ticket_types
+  validate :date_validator
+  validates :start_date, presence: true
+  def date_validator
+    errors.add(:start_date, "Must be greater than current date") if
+        start_date<Date.today
+  end
 end
