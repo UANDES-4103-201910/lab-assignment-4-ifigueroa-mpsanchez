885b50999dfbbe6fa2dbaf96016b930bfe14ff21
ignaciofigueroa <ijfigueroa@miuandes.cl>
2019-04-03 08:09:02 -0300

    Ticket validations ready. Pending of rev

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,7 +1,7 @@
 class Ticket < ApplicationRecord
   belongs_to :ticket_type
   belongs_to :order
-
+  before_save :Date_validation
   def Date_validation
     if Order.find(order_id).created_at>Event.find(event_id).start_date
       throw :abort
