24a91dd52f54c9d8a5ed5513dddeb4efdb6bddaf
ignaciofigueroa <ijfigueroa@miuandes.cl>
2019-04-03 11:58:30 -0300

    Fixed everything

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,10 +3,13 @@ class Ticket < ApplicationRecord
   belongs_to :order
   before_save :Date_validation
   def Date_validation
-    if Order.find(order_id).created_at>Event.find(event_id).start_date
+    event_start_date = Event.find(TicketType.find(ticket_type_id).event_id).start_date
+    if Order.find(order_id).created_at>event_start_date
+      puts("The ticket cannot be created after the start date of the associated event")
       throw :abort
     end
-    if Event.find(TicketType.find(ticket_type_id).event_id).start_date>created_at
+    if event_start_date >= Date.today
+      puts("The ticket can't be bought after the start date of the event")
       throw :abort
     end
   end
