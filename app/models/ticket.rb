ad113f6571100b0bf66b63062e5af597dad1496f
ignaciofigueroa <ijfigueroa@miuandes.cl>
2019-04-03 08:08:08 -0300

    Event at the same venue and date validation ready

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,13 @@
 class Ticket < ApplicationRecord
   belongs_to :ticket_type
   belongs_to :order
+
+  def Date_validation
+    if Order.find(order_id).created_at>Event.find(event_id).start_date
+      throw :abort
+    end
+    if Event.find(event_id).start_date>created_at
+      throw :abort
+    end
+  end
 end
