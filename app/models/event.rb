e393bc42304ea51dd5189ff28203e1b471af2cbb
ignaciofigueroa <ijfigueroa@miuandes.cl>
2019-04-03 07:37:04 -0300

    Event at the same venue and date validation ready

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,5 +6,7 @@ class Event < ApplicationRecord
   def date_validator
     errors.add(:start_date, "Must be greater than current date") if
         start_date<Date.today
+    errors.add("This event can't be today because there is another in the same venue at the same date") if
+        Event.where(event_venue_id: event_venue_id, start_date: start_date).count()>0
   end
 end
